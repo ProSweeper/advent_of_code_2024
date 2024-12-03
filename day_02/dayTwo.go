7defb89d7797a8ecd6460cbad8e4b68cfd9022f3
ddoucet <ddoucet@local.sequiter.com>
2024-12-03 15:37:56 -0700

    day two progress

--- day_02/dayTwo.go
+++ day_02/dayTwo.go
@@ -1,9 +1,30 @@
 package day02
 
 import (
-// utils "github.com/prosweeper/advent_of_code_2024/utils"
+	utils "github.com/prosweeper/advent_of_code_2024/utils"
+	"log"
 )
 
 func IsSafe(allowedDeltas, deltas []int) bool {
 	return false
+}
+
+func IsSafeIncrement(allowedDeltas []int, num1, num2 int) bool {
+	diff := num2 - num1
+	if diff == 0 {
+		return false
+	}
+
+	return false
+}
+
+func SolvePartOne(file string) int {
+	err, reports := utils.ReadFileByLine(file)
+	var allowedDeltas = []int{1, 2, 3}
+	if err != nil {
+		log.Fatal(err)
+	}
+	safeReports := 0
+
+	return safeReports
 }
--- day_02/dayTwo_test.go
+++ day_02/dayTwo_test.go
@@ -4,18 +4,93 @@ import (
 	"testing"
 )
 
-func TestIsSafe(t *testing.T) {
-	allowedDeltas := []int{1, 2, 3}
-	inputOne := []int{7, 6, 4, 2, 1}
-	expectedOne := true
-	actualOne := IsSafe(allowedDeltas, inputOne)
-	if expectedOne != actualOne {
-		t.Errorf("got %t, want %t", actualOne, expectedOne)
-	}
-	inputTwo := []int{1, 2, 7, 8, 9}
-	expectedTwo := false
-	actualTwo := IsSafe(allowedDeltas, inputTwo)
-	if expectedTwo != actualTwo {
-		t.Errorf("got %t, want %t", actualTwo, expectedTwo)
+var allowedDeltas = []int{1, 2, 3}
+
+func TestSolvePartOne(t *testing.T) {
+	path := "./sample.txt"
+	got := SolvePartOne(path)
+	want := 2
+
+	if got != want {
+		t.Errorf("got %d, want %d", got, want)
 	}
 }
+func TestIsSafe(t *testing.T) {
+	allowedDeltas := []int{1, 2, 3}
+
+	testCases := []struct {
+		input    []int
+		expected bool
+		name     string
+	}{
+		{
+			input:    []int{7, 6, 4, 2, 1},
+			expected: true,
+			name:     "decreasing by 1 or 2",
+		},
+		{
+			input:    []int{1, 2, 7, 8, 9},
+			expected: false,
+			name:     "increasing by 5",
+		},
+		{
+			input:    []int{9, 7, 6, 2, 1},
+			expected: false,
+			name:     "decreasing by 4",
+		},
+		{
+			input:    []int{1, 3, 2, 4, 5},
+			expected: false,
+			name:     "changing direction",
+		},
+		{
+			input:    []int{8, 6, 4, 4, 1},
+			expected: false,
+			name:     "no change between 4 4",
+		},
+		{
+			input:    []int{1, 3, 6, 7, 9},
+			expected: true,
+			name:     "increasing by 1, 2, or 3",
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			actual := IsSafe(allowedDeltas, tc.input)
+			if actual != tc.expected {
+				t.Errorf("got %t, want %t for input %v", actual, tc.expected, tc.input)
+			}
+		})
+	}
+}
+
+// func TestIsSafeIncrement(t *testing.T) {
+// 	isSafeTests := []struct {
+// 		num1         int
+// 		num2         int
+// 		isIncreasing bool
+// 		want         bool
+// 		testCase     string
+// 	}{
+// 		{1, 2, (false), false, "one"},
+// 		{1, 2, false, true, "two"},
+// 		{2, 1, false, true, "three"},
+// 		{2, 1, (false), true, "four"},
+// 		{1, 2, (true), true, "five"},
+// 		{2, 1, (true), false, "six"},
+// 		{2, 2, (true), false, "seven"},
+// 		{2, 2, (false), false, "eight"},
+// 		{2, 9, (false), false, "nine"},
+// 		{2, 9, (true), false, "ten"},
+// 		{9, 2, (true), false, "eleven"},
+// 		{9, 2, (false), false, "twelve"},
+// 	}
+// 	for _, test := range isSafeTests {
+// 		got := IsSafeIncrement(allowedDeltas, &status{true, test.isIncreasing}, test.num1, test.num2)
+//
+// 		if got != test.want {
+// 			t.Errorf("got %t, want %t  going from %d -> %d, increasing == %v case: %s", got, test.want, test.num1, test.num2, test.isIncreasing, test.testCase)
+// 		}
+// 	}
+// }
